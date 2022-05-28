f0f22479ee55328e93b4a7fbcec3c3cef23809c5
Testerbens <105905408+Testerbens@users.noreply.github.com>
2022-05-29 06:35:36 +0700

    Add files via upload

---
+++ Device.sh
@@ -0,0 +1,17 @@
+clear
+sleep 3 
+echo Ram=300mb/1000mb Storage=700mb/1000mb 
+sleep 1
+echo Cpu=78%/100% Program= Bash-Linux-Ubuntu
+echo
+echo
+echo
+echo //////////////////
+echo // Pings: 27ms  //
+echo // Speed - - - -//
+echo // Up: 3.79mb/s //
+echo // down: 780kb/s//
+echo //////////////////
+echo 
+echo
+echo
---
+++ Start.sh
@@ -0,0 +1,36 @@
+clear
+sleep 1
+echo 
+echo ----------Login-------------
+echo
+echo /////////////////////////////
+echo // -----WELCOME BACK ------//
+read -p "//Username:" Ts
+echo // PASSWORD: ••••••••••••• //
+echo /////////////////////////////
+echo
+sleep 2
+clear
+echo
+echo ////////////////////////////////
+echo //---------Setting------------//
+read -p "//Leangue: " Indonesian
+echo //Customitation Speed---------//
+read -p "//Speed: " Faster
+echo //Program: Bash-Linux-Ubuntu--//
+echo ///////////////////////////////
+echo
+echo Loading..........
+sleep 5
+clear
+echo //////////////////////////////////
+echo // LOGIN To Device Please Wait //
+sleep 3
+read -p "//Device Id: "
+echo // Login Account Id!!!!!!!!!!! //
+echo // - - - - - - - - - - - - - - //
+read -p "//Account Id: "
+echo // - - - - - - - - - - - - - - //
+echo /////////////////////////////////
+sleep 5
+chmod +x Device.sh && ./Device.sh
